1d18fa1eaba5816a076a253dd6b9172c4f991d16
Leon <32925536+LnBdd@users.noreply.github.com>
2025-04-24 11:15:24 +0200

    Update _uebung_2.py
    Updated file format

--- curac_utils/_uebung_2.py
+++ curac_utils/_uebung_2.py
@@ -21,7 +21,7 @@ class Uebung_2():
         urllib.request.urlretrieve(
             "https://upload.wikimedia.org/wikipedia/commons/d/d7/Histo_Lungenpest.jpg", "Histo_Lungenpest.jpg")
 
-        img = img_as_ubyte(io.imread("Histo_Lungenpest.png", as_gray=False))
+        img = img_as_ubyte(io.imread("Histo_Lungenpest.jpg", as_gray=False))
 
         return img[:, :, 0], img[:, :, 1], img[:, :, 2]
 
